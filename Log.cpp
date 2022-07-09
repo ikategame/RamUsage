9b3fab9c9dbcb9929d09d420d74f1d712cffc070
Catalina <eunightcoredesire@gmail.com>
2022-07-09 14:09:23 +0300

    Add project files.

---
+++ Log.cpp
@@ -0,0 +1,6 @@
+#include "includes.h"
+
+void Log(const char* string)
+{
+	std::cout << string << std::endl;
+}
---
+++ Log.h
@@ -0,0 +1,5 @@
+#ifndef _LOG_H_
+#define _LOG_H_
+void Log(const char* string);
+
+#endif
